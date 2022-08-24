18babae570c75bcb703656ccfe186d1871422b67
Bojan Nikolić <7010347+nikolic-bojan@users.noreply.github.com>
2022-08-24 13:04:47 +0200

    test5

--- k8s-dev/src/api/Program.cs
+++ k8s-dev/src/api/Program.cs
@@ -8,6 +8,6 @@ builder.Services.AddCors(options => {
 var app = builder.Build();
 app.UseCors();
 
-app.MapGet("/", () => Results.Ok(new { message = "Hello World 2.55!"}));
+app.MapGet("/", () => Results.Ok(new { message = "Hello World 2.56!"}));
 
 app.Run();
