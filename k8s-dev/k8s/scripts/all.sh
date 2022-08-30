a607a86850d385f61c5aa9e9efa7fc079834eddb
Bojan Nikolić <bojan.nikolic@symphony.is>
2022-08-30 16:31:39 +0200

    Adjusted Kustomization scripts and created Bash scripts for setting up Local environment and also nukeing it.

---
+++ k8s-dev/k8s/scripts/all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+kubectl apply -f $parent_path/../base/namespace.yaml
+kubectl apply -f $parent_path/../base/dockerconfigjson.yaml
+
+kubectl apply -f $parent_path/../base/db-data.yaml
+kubectl apply -f $parent_path/../base/db.yaml
+
+kubectl apply -f $parent_path/../base/api.yaml
+kubectl apply -f $parent_path/../base/ui.yaml
+
+kubectl apply -f $parent_path/../base/ingress.yaml
+
+kubectl -n local rollout restart deployment/api-deployment
+kubectl -n local rollout restart deployment/ui-deployment
---
+++ k8s-dev/k8s/scripts/nuke.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+kubectl delete -f $parent_path/../base/ingress.yaml
+
+kubectl delete -f $parent_path/../base/db.yaml
+
+kubectl delete -f $parent_path/../base/api.yaml
+kubectl delete -f $parent_path/../base/ui.yaml
+
+kubectl delete -f $parent_path/../base/db-data.yaml
+
+kubectl delete -f $parent_path/../base/dockerconfigjson.yaml
+kubectl delete -f $parent_path/../base/namespace.yaml
